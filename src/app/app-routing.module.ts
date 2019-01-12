84d41601cd45c0a9e18e258c740f99d13e1a8414
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-01-12 11:46:02 +0530

    create project and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  { path: 'people', loadChildren: './pages/people/people.module#PeoplePageModule' },
+  { path: 'planets', loadChildren: './pages/planets/planets.module#PlanetsPageModule' },
+  { path: 'films', loadChildren: './pages/films/films.module#FilmsPageModule' },
+  { path: 'film-details', loadChildren: './pages/film-details/film-details.module#FilmDetailsPageModule' },
 ];
 
 @NgModule({
